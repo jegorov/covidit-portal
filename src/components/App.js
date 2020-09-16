93286bca4acb1c9c0aee2f4ecdc1ef3c34990076
Egor Egorov <egorov.dev@yahoo.com>
2020-09-17 01:03:33 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "./Login/SignIn";
+import Register from "./Register/Register";
+
+export default function App() {
+    return (
+       <div>
+           <Register/>
+       </div>
+    );
+}
